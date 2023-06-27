5b561da06d7521ad570426b76f2e613ccc294439
Utkarsh Choudhary <choudharyu2003@gmail.com>
2023-06-27 21:45:29 +0530

    Bad file struct

---
+++ securecapita/src/test/java/io/getarrays/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getarrays.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
